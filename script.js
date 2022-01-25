1011a75b6404bd640153f2e5694a3870066c66f3
joaquin <jecorrea@protonmail.com>
2022-01-24 23:22:08 -0500

    events firing

--- script.js
+++ script.js
@@ -3,6 +3,18 @@
 const fill = document.querySelector(".fill");
 const empties = document.querySelectorAll(".empty");
 
+// Add the event for the filled box
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+// Add the events for the empty boxes
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
 function dragStart() {
   console.log("drag start");
 }
