3e4a6cd94423580af079bf9ce0d7d1d6499cb1f2
joaquin <jecorrea@protonmail.com>
2022-01-24 23:08:36 -0500

    basic functions

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 
 const fill = document.querySelector(".fill");
 const empties = document.querySelectorAll(".empty");
+
+function dragStart() {
+  console.log("drag start");
+}
+
+function dragEnd() {
+  console.log("drag end");
+}
+
+function dragOver(e) {
+  console.log("drag over");
+}
+
+function dragEnter(e) {
+  console.log("drag enter");
+}
+
+function dragLeave() {
+  console.log("drag leave");
+}
+
+function dragDrop() {
+  console.log("drag drop");
+}
